0a26f3127b533e41ad2469e5591119122fe0a789
dostondevop <dastonbekofficial@gmail.com>
2025-02-01 21:44:14 +0500

    Project is uploaded

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+import menu_icon from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+        <div className='nav-left flex-div'>
+            <img className='menu-icon' src={menu_icon} alt="" />
+            <img className='logo' src={logo} alt="" />
+        </div>
+
+        <div className='nav-middle flex-div'>
+            <input type="text" placeholder='Search' />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
