601b942835020885593adf3d0d93f52af62546e6
dostondevop <dastonbekofficial@gmail.com>
2025-02-02 23:42:54 +0500

    Navbar structure built

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Video from './pages/video/Video'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:vieoId' element={<Video/>} />
+      </Routes>
     </div>
   )
 }
