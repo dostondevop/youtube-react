bb98fd1139e6ca74c8e9a048e0ccfeac0ab5fb7f
dostondevop <dastonbekofficial@gmail.com>
2025-02-03 23:36:32 +0500

    Sidebar and Feed added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Home.css'
+import Sidebar from '../../components/sidebar/Sidebar.jsx'
+import Feed from '../../components/feed/Feed.jsx'
 
-const Home = () => {
+const Home = ({sidebar}) => {
   return (
     <>
-        <Sidebar/>
+        <Sidebar sidebar = {sidebar}/>
+        <div className={`container ${sidebar ? "" : 'large-conatiner'}`}>
+            <Feed/>
+        </div>
     </>
   )
 }
