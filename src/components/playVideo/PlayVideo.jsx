255273deb9fa3dd53caf08b2433397a2848c3f08
dostondevop <dastonbekofficial@gmail.com>
2025-02-08 21:54:03 +0500

    PlayVideo component added and Feed is upgraded

---
+++ src/components/playVideo/PlayVideo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './playVideo.css'
+import video1 from '../../assets/video.mp4'
+
+const PlayVideo = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PlayVideo
